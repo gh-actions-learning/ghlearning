e3fcba131e2efed363822d7f596bbb1b74c493fc
rich <rich@rich.com>
2025-12-22 09:07:54 +0530

    modify file

--- src/apps/app.py
+++ src/apps/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask in Docker!."
+    return "Hello RICH from Flask in Docker!."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
