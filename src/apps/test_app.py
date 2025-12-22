d5303dac74822ccd599c5031a75d23cfd381b925
rich <rich@rich.com>
2025-12-22 09:09:45 +0530

    modifyed assert value

--- src/apps/test_app.py
+++ src/apps/test_app.py
@@ -4,4 +4,4 @@ def test_home():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert b"Hello from Flask in Docker!" in response.data
+    assert b"Hello RICH from Flask in Docker!." in response.data
